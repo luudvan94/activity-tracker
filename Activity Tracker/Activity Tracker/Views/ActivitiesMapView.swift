cb4ec79d6717295e04fe9fb7d596aa2a252d615c
luudvan94 <33990794+luudvan94@users.noreply.github.com>
2022-03-04 10:59:19 -0600

    add mapkit extension

---
+++ Activity Tracker/Activity Tracker/Views/ActivitiesMapView.swift
@@ -0,0 +1,21 @@
+//
+//  ActivitiesMapView.swift
+//  Activity Tracker
+//
+//  Created by luu van on 3/4/22.
+//
+
+import SwiftUI
+
+struct ActivitiesMapView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActivitiesMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivitiesMapView()
+    }
+}
